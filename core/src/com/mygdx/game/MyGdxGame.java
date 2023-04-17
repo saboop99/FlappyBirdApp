fd2eed93ecff10c4eb919449e0b06cfb9fae9f61
saboop99 <rafa4021@hotmail.com>
2023-04-17 20:59:03 -0300

    adição das variáveis

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,13 +1,60 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Preferences;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import java.util.Random;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
+	private Texture[] passaros;
+	private Texture fundo;
+	private Texture canoBaixo;
+	private Texture canoTopo;
+	private Texture gameOver;
+
+	private ShapeRenderer shapeRenderer;
+	private Circle circuloPassaro;
+	private Rectangle retanguloCanoCima;
+	private Rectangle rentaguloCanoBaixo;
+
+	private float larguraDispositivo;
+	private float alturaDispositivo;
+	private float variacao = 0;
+	private float gravidade = 2;
+	private float posiçãoInicialVerticalPassaro = 0;
+	private float posiçãoCanoHorizontal;
+	private float posiçãoCanoVertical;
+	private float espaçoEntreCanos;
+	private Random random;
+	private int pontos = 0;
+	private int pontuaçãoMáxima = 0;
+	private boolean passsouCano = false;
+
+	BitmapFont textoPontuação;
+	BitmapFont textoReiniciar;
+	BitmapFont textoMelhorPontuação;
+
+	Sound somVoando;
+	Sound somColisão;
+	Sound somPontuação;
+
+	Preferences preferencias;
+
+	private OrthographicCamera camera;
+	private Viewport viewport;
+	private final float VIRTUAL_WIDTH = 720;
+	private final float VIRTUAL_HEIGHT = 1280;
 	
 	@Override
 	public void create () {
